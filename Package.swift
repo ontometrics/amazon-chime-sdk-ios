1bda7cb7b753ee93eaf78771ff5f17c628c372ed
David Krystall <david.krystall@ontometrics.com>
2021-11-24 12:38:56 -0800

    removing checksum, archive cannot be downloaded from s3 for some reason

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AmazonChimeSDK",
-            url: "https://amazonchime-onto.s3.amazonaws.com/AmazonChimeSDK.xcframework.zip",
-            checksum: "e371d250d199136728b41605cdc21cdbf23b05e70a88c99c59ae25f29b6c094d"
+            url: "https://amazonchime-onto.s3.amazonaws.com/AmazonChimeSDK.xcframework.zip"
+            //checksum: "`e371d250d199136728b41605cdc21cdbf23b05e70a88c99c59ae25f29b6c094d"
         ),
         .binaryTarget(
             name: "AmazonChimeSDKMedia",
-            url: "https://amazonchime-onto.s3.amazonaws.com/AmazonChimeSDKMedia.xcframework.zip",
-            checksum: "ea602f96812010f9baab72bb16bf0e6398515cbb4054345a8373e0606c8860f3"
+            url: "https://amazonchime-onto.s3.amazonaws.com/AmazonChimeSDKMedia.xcframework.zip
+            //checksum: "ea602f96812010f9baab72bb16bf0e6398515cbb4054345a8373e0606c8860f3"
         )
     ]
 )

3555bcace4e03061732c33eb702e9b23a0408da6
eduardo <rivera.eduardo@berkeley.edu>
2021-11-24 16:46:23 -0800

    Added iOS platform

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "AmazonChimeSDK",
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "AmazonChimeSDK", targets: ["AmazonChimeSDK", "AmazonChimeSDKMedia"])
     ],
